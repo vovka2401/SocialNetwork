edb0c636c4f494d0930804f5d333b8d34ce11a57
vovka2401 <113299727+vovka2401@users.noreply.github.com>
2023-09-22 14:11:19 +0300

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by Volodymyr Khvaliuk on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
