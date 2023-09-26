27600bae592782e164928fe12ea7a647e56ae4e9
vovka2401 <113299727+vovka2401@users.noreply.github.com>
2023-09-26 16:15:44 +0300

    create new message view + view model added

---
+++ SocialNetwork/ViewModels/CreateNewMessageViewModel.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+class CreateNewMessageViewModel: ObservableObject {
+    @Published var users = [ChatUser]()
+    @Published var errorMessage = ""
+
+    init() {
+        fetchAllUsers()
+    }
+
+    private func fetchAllUsers() {
+        FirebaseManager.shared.firestore.collection("users")
+            .getDocuments { documentsSnapshot, error in
+                if let error = error {
+                    self.errorMessage = "Failed to fetch users: \(error)"
+                    print("Failed to fetch users: \(error)")
+                    return
+                }
+                documentsSnapshot?.documents.forEach({ snapshot in
+                    let user = try? snapshot.data(as: ChatUser.self)
+                    if user?.uid != FirebaseManager.shared.auth.currentUser?.uid {
+                        self.users.append(user!)
+                    }
+                    
+                })
+            }
+    }
+}
